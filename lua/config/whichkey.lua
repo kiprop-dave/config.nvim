c2b375eb25c09d7080a6f961dcca3e9fecfe0968
kiprop-dave <davekip16@gmail.com>
2023-07-25 17:13:29 +0300

    added telescope search current buffer

--- lua/config/whichkey.lua
+++ lua/config/whichkey.lua
@@ -5,22 +5,22 @@ end
 
 local setup = {
   plugins = {
-    marks = true,             -- shows a list of your marks on ' and `
-    registers = true,         -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+    marks = true,       -- shows a list of your marks on ' and `
+    registers = true,   -- shows your registers on " in NORMAL or <C-r> in INSERT mode
     spelling = {
-      enabled = true,         -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-      suggestions = 20,       -- how many suggestions should be shown in the list?
+      enabled = true,   -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+      suggestions = 20, -- how many suggestions should be shown in the list?
     },
     -- the presets plugin, adds help for a bunch of default keybindings in Neovim
     -- No actual key bindings are created
     presets = {
-      operators = false,         -- adds help for operators like d, y, ... and registers them for motion / text object completion
-      motions = true,            -- adds help for motions
-      text_objects = true,       -- help for text objects triggered after entering an operator
-      windows = true,            -- default bindings on <c-w>
-      nav = true,                -- misc bindings to work with windows
-      z = true,                  -- bindings for folds, spelling and others prefixed with z
-      g = true,                  -- bindings for prefixed with g
+      operators = false,   -- adds help for operators like d, y, ... and registers them for motion / text object completion
+      motions = true,      -- adds help for motions
+      text_objects = true, -- help for text objects triggered after entering an operator
+      windows = true,      -- default bindings on <c-w>
+      nav = true,          -- misc bindings to work with windows
+      z = true,            -- bindings for folds, spelling and others prefixed with z
+      g = true,            -- bindings for prefixed with g
     },
   },
   -- add operators that will trigger motion and text object completion
@@ -34,31 +34,31 @@ local setup = {
     -- ["<tab>"] = "TAB",
   },
   icons = {
-    breadcrumb = "»",    -- symbol used in the command line area that shows your active key combo
-    separator = "➜",    -- symbol used between a key and it's label
-    group = "+",          -- symbol prepended to a group
+    breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+    separator = "➜", -- symbol used between a key and it's label
+    group = "+",      -- symbol prepended to a group
   },
   popup_mappings = {
-    scroll_down = "<c-d>",     -- binding to scroll down inside the popup
-    scroll_up = "<c-u>",       -- binding to scroll up inside the popup
+    scroll_down = "<c-d>", -- binding to scroll down inside the popup
+    scroll_up = "<c-u>",   -- binding to scroll up inside the popup
   },
   window = {
-    border = "rounded",           -- none, single, double, shadow
-    position = "bottom",          -- bottom, top
-    margin = { 1, 0, 1, 0 },      -- extra window margin [top, right, bottom, left]
-    padding = { 2, 2, 2, 2 },     -- extra window padding [top, right, bottom, left]
+    border = "rounded",       -- none, single, double, shadow
+    position = "bottom",      -- bottom, top
+    margin = { 1, 0, 1, 0 },  -- extra window margin [top, right, bottom, left]
+    padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
     winblend = 0,
   },
   layout = {
-    height = { min = 4, max = 25 },                                               -- min and max height of the columns
-    width = { min = 20, max = 50 },                                               -- min and max width of the columns
-    spacing = 3,                                                                  -- spacing between columns
-    align = "left",                                                               -- align columns left, center or right
+    height = { min = 4, max = 25 },                                             -- min and max height of the columns
+    width = { min = 20, max = 50 },                                             -- min and max width of the columns
+    spacing = 3,                                                                -- spacing between columns
+    align = "left",                                                             -- align columns left, center or right
   },
-  ignore_missing = true,                                                          -- enable this to hide mappings for which you didn't specify a label
-  hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " },   -- hide mapping boilerplate
-  show_help = true,                                                               -- show help message on the command line when the popup is visible
-  triggers = "auto",                                                              -- automatically setup triggers
+  ignore_missing = true,                                                        -- enable this to hide mappings for which you didn't specify a label
+  hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
+  show_help = true,                                                             -- show help message on the command line when the popup is visible
+  triggers = "auto",                                                            -- automatically setup triggers
   -- triggers = {"<leader>"} -- or specify a list manually
   triggers_blacklist = {
     -- list of mode / prefixes that should never be hooked by WhichKey
@@ -70,12 +70,12 @@ local setup = {
 }
 
 local opts = {
-  mode = "n",       -- NORMAL mode
+  mode = "n",     -- NORMAL mode
   prefix = "<leader>",
-  buffer = nil,     -- Global mappings. Specify a buffer number for buffer local mappings
-  silent = true,    -- use `silent` when creating keymaps
-  noremap = true,   -- use `noremap` when creating keymaps
-  nowait = true,    -- use `nowait` when creating keymaps
+  buffer = nil,   -- Global mappings. Specify a buffer number for buffer local mappings
+  silent = true,  -- use `silent` when creating keymaps
+  noremap = true, -- use `noremap` when creating keymaps
+  nowait = true,  -- use `nowait` when creating keymaps
 }
 
 local mappings = {
@@ -105,7 +105,7 @@ local mappings = {
     name = "File Search",
     c = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
     f = { "<cmd>lua require('telescope.builtin').find_files()<cr>", "Find files" },
-    t = { "<cmd>Telescope live_grep <cr>", "Find Text Pattern In All Files" },
+    g = { "<cmd>Telescope live_grep <cr>", "Find Text Pattern In All Files" },
     r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
     b = { "<cmd> lua require('telescope.builtin').buffers()<cr>", "Find buffers" }
   },
@@ -117,19 +117,20 @@ local mappings = {
     r = { "<cmd>Telescope registers<cr>", "Registers" },
     k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
     c = { "<cmd>Telescope commands<cr>", "Commands" },
+    b = { "<cmd>Telescope current_buffer_fuzzy_find<cr>", "Current Buffer" },
   },
 
   --Toggle Term
   t = {
     name = "Terminal",
-    n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" },     -- Node Terminal
+    n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" }, -- Node Terminal
     -- t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" }, -- (Optional) Htop, If you have htop in linux
     -- p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" }, -- Python Terminal
-    f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },     -- Floating Terminal
+    f = { "<cmd>ToggleTerm direction=float<cr>", "Float" }, -- Floating Terminal
 
     -- Play with size according to your needs.
-    h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },     -- Horizontal Terminal,
-    v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },         -- Vertical Terminal
+    h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" }, -- Horizontal Terminal,
+    v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },     -- Vertical Terminal
   },
 
   d = {
